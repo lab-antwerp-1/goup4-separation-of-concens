f704b080f4b9874e657251d92b091cd2dfe07dba
Alina Rao <88941807+AlinaTaoRao@users.noreply.github.com>
2022-02-25 11:29:58 +0100

    stop using template literal

--- client/src/handlers/remove-item.js
+++ client/src/handlers/remove-item.js
@@ -23,7 +23,7 @@ export const removeListHandler = (event) => {
   const title = event.target.parentElement.children[indexOfTitle].textContent;
 
   // update state
-  delete data[`${title}`];
+  delete data[title];
 
   // update the UI
   const newList = renderList(Object.keys(data));
