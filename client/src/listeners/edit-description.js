ed9f295d1e30f02bbb53410f50d148c44de88c20
Aviv <aviv.dror@gmail.com>
2022-02-28 01:53:27 +0100

    edit listeners

--- client/src/listeners/edit-description.js
+++ client/src/listeners/edit-description.js
@@ -1,3 +1,9 @@
 import { newDescription } from '../handlers/new-description.js';
+import { editDescription } from '../handlers/edit-description.js';
 
 document.getElementById('display').addEventListener('click', newDescription);
+
+document.getElementById('display').addEventListener('click', editDescription);
+document
+  .getElementById('display')
+  .removeEventListener('click', editDescription);
