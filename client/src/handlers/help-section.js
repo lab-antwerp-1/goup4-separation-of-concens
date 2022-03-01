790d47b38ea3ffb3c005fa987843c5ae5e4d327e
Aviv <aviv.dror@gmail.com>
2022-03-01 05:44:21 +0100

    spelling fix

--- client/src/handlers/help-section.js
+++ client/src/handlers/help-section.js
@@ -16,9 +16,9 @@ to add items to your list and the 'remove' button to edit items
 out.<br />Click the 'reset' button to clear your list and start
 over.<br /><br />In order to add descriptions to your items click the
 'description' button.<br />You can also check off items by using the
-checkboxes.<br /><br />Once your list is complete you can choose how
+check-boxes.<br /><br />Once your list is complete you can choose how
 you would like to see the items displayed with the 'sort list'
-dropbox.<br />You can also stylize your items using the 'stylize'
+drop-box.<br />You can also stylize your items using the 'stylize'
 button.<br /><br />Enjoy making your lists and check back for new
 features in the future!`;
 
