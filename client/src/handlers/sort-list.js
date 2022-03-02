658f95fcc86bd4d8f9601fe0a97459f008f8cfd2
Aviv <aviv.dror@gmail.com>
2022-03-02 03:33:09 +0100

    first handler

--- client/src/handlers/sort-list.js
+++ client/src/handlers/sort-list.js
@@ -2,34 +2,33 @@ export const showSort = (event) => {
   if (event.target.id !== 'sort') {
     return;
   }
-  console.log('goop', event.target.id);
-  /*
-  const isId = document.getElementById('intro');
-  const instructions = document.createElement('p');
-  instructions.id = 'instructions';
-  instructions.classList.add('instructions');
 
-  instructions.innerHTML = `Welcome to list maker!<br />Our page will help you stay on top of your
-lists.<br />
-To get started simply type your first item in the box below and a new
-list will be created automatically.<br /><br />Click the 'add' button
-to add items to your list and the 'remove' button to edit items
-out.<br />Click the 'reset' button to clear your list and start
-over.<br /><br />In order to add descriptions to your items click the
-'description' button.<br />You can also check off items by using the
-check-boxes.<br /><br />Once your list is complete you can choose how
-you would like to see the items displayed with the 'sort list'
-drop-box.<br />You can also stylize your items using the 'stylize'
-button.<br /><br />Enjoy making your lists and check back for new
-features in the future!`;
+  const displayEl = document.getElementById('display');
+  const tEl = document.getElementById('display-table');
 
-  if (isId.children.length === 1) {
-    isId.appendChild(instructions);
+  const sortContainer = document.createElement('div');
+  sortContainer.id = 'sort-container';
+  const sortLabel = document.createElement('label');
+  sortLabel.id = 'sort-label';
+  sortLabel.for = 'select';
+  sortLabel.innerHTML = 'Sort List By:';
+
+  const sortSelect = document.createElement('select');
+  sortSelect.id = 'sort-select';
+  sortSelect.innerHTML = `<option value="new">First to Last</option>
+  <option value="old">Last to First</option>
+  <option value="a">Alphabetical</option>
+  <option value="z">Reverse Alphabetical</option>
+  <option value="short">Length</option>
+  <option value="long">Reverse Length</option>`;
+  sortContainer.appendChild(sortLabel);
+  sortContainer.appendChild(sortSelect);
+
+  if (displayEl.children.length === 1) {
+    displayEl.insertBefore(sortContainer, tEl);
     return;
   }
-
-  if (isId.children.length === 2) {
-    isId.removeChild(isId.children[1]);
+  if (displayEl.children.length === 2) {
+    displayEl.removeChild(displayEl.children[0]);
   }
-  */
 };
