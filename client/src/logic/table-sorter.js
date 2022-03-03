c7dd4f22e44f9b330be7b4c7e7248835803a8156
Aviv <aviv.dror@gmail.com>
2022-03-03 04:53:00 +0100

    docjs

--- client/src/logic/table-sorter.js
+++ client/src/logic/table-sorter.js
@@ -1,3 +1,25 @@
+/**
+ * Sorts an array of strings in different ways.
+ * It does not modify the argument (no side-effects).
+ *
+ * @param {string[]} [toSort=[]] - The array of strings to sort.
+ * @param {string} [sortType='new'] - How to sort the strings, 6 options.
+ * - old: from oldest to newest.
+ * - new: from newest to oldest.
+ * - short: from shortest to longest.
+ * - long: from longest to shortest.
+ * - a: alphabetical order.
+ * - z: reverse alphabetical order.
+ * If the sortType is not one of these 6 options, a copy of toSort is returned.
+ * @returns {string[]} A new sorted array containing the same strings as toSort.
+ * @example
+ *
+ * // [Monday, Tuesday, Wednesday, Thursday], 'short' --> [Monday, Tuesday, Thursday, Wednesday]
+ * @example
+ *
+ * // [Monday, Tuesday, Wednesday, Thursday], 'z' --> [Wednesday, Tuesday, Thursday, Monday]
+ */
+
 export const tableSorter = (toSort = [], sortType = '') => {
   const tempArray = [...toSort];
 
