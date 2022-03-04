6ff8d7af75f74ba09fb6424cb3dc92a14c18abd8
Aviv <aviv.dror@gmail.com>
2022-03-04 04:40:49 +0100

    setup bin svg in component

--- client/src/components/render-table.js
+++ client/src/components/render-table.js
@@ -52,10 +52,26 @@ export const renderTable = (itemToRender = [], descriptionToRender = []) => {
     itemTrEl.id = `item-row-${index}`;
     itemTdEl.id = `item-td-${index}`;
     itemTdEl.classList.add('item-td');
-    const removeImgEl = document.createElement('img');
+    /* const removeImgEl = document.createElement('img');
     removeImgEl.src = 'client/public/trash-2.png';
     removeImgEl.id = `remove-${index}`;
-    removeImgEl.className = 'delete-btn';
+    removeImgEl.className = 'delete-btn'; */
+
+    const removeImgEl = document.createElement('i');
+    removeImgEl.innerHTML = `<svg class="delete-btn" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2.5 5H4.16667H17.5" stroke="#BC0808" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M6.66667 5.00001V3.33334C6.66667 2.89131 6.84226 2.46739 7.15482 2.15483C7.46738 1.84227 7.89131 1.66667 8.33333 1.66667H11.6667C12.1087 1.66667 12.5326 1.84227 12.8452 2.15483C13.1577 2.46739 13.3333 2.89131 13.3333 3.33334V5.00001M15.8333 5.00001V16.6667C15.8333 17.1087 15.6577 17.5326 15.3452 17.8452C15.0326 18.1577 14.6087 18.3333 14.1667 18.3333H5.83333C5.39131 18.3333 4.96738 18.1577 4.65482 17.8452C4.34226 17.5326 4.16667 17.1087 4.16667 16.6667V5.00001H15.8333Z" stroke="#BC0808" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M8.33333 9.16667V14.1667" stroke="#BC0808" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M11.6667 9.16667V14.1667" stroke="#BC0808" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>`;
+    removeImgEl.children[0].id = `remove-${index}`;
+
+    /* <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M2.5 5H4.16667H17.5" stroke="#BC0808" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M6.66667 5.00001V3.33334C6.66667 2.89131 6.84226 2.46739 7.15482 2.15483C7.46738 1.84227 7.89131 1.66667 8.33333 1.66667H11.6667C12.1087 1.66667 12.5326 1.84227 12.8452 2.15483C13.1577 2.46739 13.3333 2.89131 13.3333 3.33334V5.00001M15.8333 5.00001V16.6667C15.8333 17.1087 15.6577 17.5326 15.3452 17.8452C15.0326 18.1577 14.6087 18.3333 14.1667 18.3333H5.83333C5.39131 18.3333 4.96738 18.1577 4.65482 17.8452C4.34226 17.5326 4.16667 17.1087 4.16667 16.6667V5.00001H15.8333Z" stroke="#BC0808" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M8.33333 9.16667V14.1667" stroke="#BC0808" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11.6667 9.16667V14.1667" stroke="#BC0808" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg> */
     const descriptionTrEl = document.createElement('tr');
     const descriptionTdEl = document.createElement('td');
     descriptionTrEl.id = `description-row-${index}`;
